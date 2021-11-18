1dec4adc86b238fd8ea24f14b393e0f397ef8a52
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-18 15:48:10 +0200

    ex6-1-Mumbling

--- ex6.1.js
+++ ex6.1.js
@@ -1,3 +1,17 @@
-function accum(){
+
+function accum(string){
+    // const array = Array(string);
+    let res = "";
     
+    for(let i=0;i<string.length;i++){
+        if(i!=0)
+            res+="-";
+        res+=string.charAt(i).toUpperCase();
+        res+= string.charAt(i).toLowerCase().repeat(i);
+    }
+    return res;
 }
+
+console.log(accum("abcd"));
+console.log(accum("RqaEzty"));
+console.log(accum("cwAt"));
