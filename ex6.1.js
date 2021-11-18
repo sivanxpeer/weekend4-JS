ef8c71a579225082003758d1a63e4afc7c349df6
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-18 16:29:11 +0200

    Mumbling-input-validation

--- ex6.1.js
+++ ex6.1.js
@@ -1,16 +1,20 @@
 
-function accum(string){
+function accum(string) {
     let res = "";
-    
-    for(let i=0;i<string.length;i++){
-        if(i!=0)
-            res+="-";
-        res+=string.charAt(i).toUpperCase();
-        res+= string.charAt(i).toLowerCase().repeat(i);
+    for (let i = 0; i < string.length; i++) {
+            if (String(string).match(/[^a-zA-Z]/)){
+                return "ivalid input";
+            }
+            if (i != 0)
+                res += "-";
+            res += string.charAt(i).toUpperCase();
+            res += string.charAt(i).toLowerCase().repeat(i);
+        }
+        return res;
     }
-    return res;
-}
+
 
 console.log(accum("abcd"));
 console.log(accum("RqaEzty"));
 console.log(accum("cwAt"));
+console.log(accum("cwAt231"));
